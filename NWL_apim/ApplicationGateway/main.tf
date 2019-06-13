c45c91424e739a3db41f3dd1a0276943b3f7ccb4
SGGIRBS <30274540+SGGIRBS@users.noreply.github.com>
2019-06-13 12:16:25 +0100

    Update main.tf

--- NWL_apim/ApplicationGateway/main.tf
+++ NWL_apim/ApplicationGateway/main.tf
@@ -72,7 +72,7 @@ resource "azurerm_application_gateway" "appgw" {
     exclusion {
       match_variable = "RequestArgNames"
       selector_match_operator = "Equals"
-      selector = "doc_query"
+      selector = "docs_query"
       }
     exclusion {
       match_variable = "RequestArgNames"
